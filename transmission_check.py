af9aaf59048aab1a96abdd1d84dedf872b66fdb1
szlaci83 <szlaci83@gmail.com>
2020-04-21 16:38:55 +0100

    added transmission svc

--- transmission_check.py
+++
@@ -1,9 +0,0 @@
-import transmissionrpc as t
-from properties import Transmission as transmission_properties
-
-tc = t.Client(**transmission_properties)
-torrents = tc.get_torrents()
-for t in torrents:
-#    print(dict(__builtins__))
-    print(t.name, t.date_added, t._status(), t.date_done)
-
---
+++ transmission_service.py
@@ -0,0 +1,16 @@
+import transmissionrpc as t
+from datetime import datetime
+from properties import Transmission as transmission_properties
+
+DEFAULT_LOOKBACK_MINS = 15
+tc = t.Client(**transmission_properties)
+
+
+def get_latest_finished(lookback_min=DEFAULT_LOOKBACK_MINS):
+    return [t for t in tc.get_torrents() if t.date_done > datetime.now() - datetime.timedelta(minutes=lookback_min)]
+
+
+if __name__ == '__main__':
+    for t in get_latest_finished(2880):
+        print(t.name)
+
