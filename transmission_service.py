ebfd4d3db00707eb098e4d4da62a338403a80e81
szlaci83 <szlaci83@gmail.com>
2020-04-21 23:03:20 +0100

    fix

--- transmission_service.py
+++ transmission_service.py
@@ -1,5 +1,5 @@
 import transmissionrpc as t
-from datetime import datetime
+from datetime import datetime, timedelta
 from properties import Transmission as transmission_properties
 
 DEFAULT_LOOKBACK_MINS = 15
@@ -7,7 +7,7 @@ tc = t.Client(**transmission_properties)
 
 
 def get_latest_finished(lookback_min=DEFAULT_LOOKBACK_MINS):
-    return [t for t in tc.get_torrents() if t.date_done > datetime.now() - datetime.timedelta(minutes=lookback_min)]
+    return [t for t in tc.get_torrents() if t.date_done > datetime.now() - timedelta(minutes=lookback_min)]
 
 
 if __name__ == '__main__':
