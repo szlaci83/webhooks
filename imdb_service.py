f31425434c12b1fd687accd57e5e85fc3f522fe9
laci <szlaci83@gmail.com>
2020-06-25 12:27:05 +0100

    fix add poster by name

--- imdb_service.py
+++ imdb_service.py
@@ -13,7 +13,7 @@ def _get_poster_url(imdb_id):
 
 def _search_for_id(search_term):
     search_term = str(search_term).replace(" ", "+")
-    search_url = f"https://www.imdb.com/find?q={search_term}&ref_=nv_sr_sm"
+    search_url = "https://www.imdb.com/find?q=%s&ref_=nv_sr_sm" % search_term
     page = requests.get(search_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     imdb_id = soup.find('td', class_='primary_photo').find('a').get('href')
@@ -34,7 +34,7 @@ def get_poster(imdb_url):
 def get_poster_by_name(torrent_title):
     movie_title = PTN.parse(torrent_title).get('title', '')
     imdb_id = _search_for_id(movie_title)
-    get_poster(f"https://www.imdb.com{imdb_id}")
+    get_poster("https://www.imdb.com%s" % imdb_id)
 
 
 if __name__ == '__main__':
