041b0e7a0ffef11f1f866aeaab63efdb4dd161c9
szlaci83 <szlaci83@gmail.com>
2020-06-25 23:36:46 +0100

    added seed stop

--- transmission_service.py
+++ transmission_service.py
@@ -3,7 +3,24 @@ from datetime import datetime, timedelta
 from properties import Transmission as transmission_properties
 
 DEFAULT_LOOKBACK_MINS = 15
+NCORE_SEED_HRS = 50
 tc = t.Client(**transmission_properties)
+
+
+def stop_seeding():
+    stopped = []
+    for torrent in tc.get_torrents():
+        for tracker in torrent.trackers:
+            # stop seeding ncore torrents after 50 hours
+            if tracker.get("announce", "").find("ncore") > 0:
+                if torrent.status == 'seeding' and torrent.date_done < datetime.now() - timedelta(hours=NCORE_SEED_HRS):
+                    torrent.stop()
+                    stopped.append(torrent)
+            # stop seeding other torrents immediately
+            elif torrent.status == 'seeding':
+                torrent.stop()
+               # stopped.append(torrent)
+    return stopped
 
 
 def get_latest_finished(lookback_min=DEFAULT_LOOKBACK_MINS):
--- transmission_watch.py
+++ transmission_watch.py
@@ -12,12 +12,13 @@ def notify_kodi(msg, update=True):
     if update:
         kodi.refresh_video_library()
 
+
 def notify_mail(msg, add_poster=False):
     imdb.get_poster_by_name(msg)
     mail.send_tr_mail_to_all(msg, add_poster=add_poster)
 
 
-def main():
+def check_downloaded():
     all_finished = transmission.get_latest_finished()
     if len(all_finished) == 0:
         return
@@ -32,6 +33,23 @@ def main():
         except Exception as e:
             print(str(e))
             notify_mail(t.name)
+
+
+def stop_seeding():
+    stopped = transmission.stop_seeding()
+    if len(stopped) == 0:
+        return
+    msg = "STOP_SEEDING:" + ", ".join([s.name for s in stopped])
+    try:
+        notify_kodi(msg)
+    except Exception as e:
+        print(str(e))
+    notify_mail(msg)
+
+
+def main():
+    check_downloaded()
+    stop_seeding()
 
 
 if __name__ == '__main__':
